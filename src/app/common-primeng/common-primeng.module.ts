8fd15c2bd2776cc19a34e7b9b7586994142605c9
ovppc <oscarvelasco.1998@gmail.com>
2024-11-27 19:35:20 +0100

    feat(Home) Create mega menu

--- src/app/common-primeng/common-primeng.module.ts
+++ src/app/common-primeng/common-primeng.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { MegaMenuModule } from 'primeng/megamenu';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MegaMenuModule
   ]
 })
 export class CommonPrimengModule { }
