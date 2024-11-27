5e1844b7097f753741978818da68ccddd2c7a3b3
ovppc <oscarvelasco.1998@gmail.com>
2024-11-27 23:52:19 +0100

    feat(Home) Update home styles

--- src/app/common-primeng/common-primeng.module.ts
+++ src/app/common-primeng/common-primeng.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MegaMenuModule } from 'primeng/megamenu';
 import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
 
 
 @NgModule({
   exports: [
     MegaMenuModule,
-    ButtonModule
+    ButtonModule,
+    ImageModule
   ]
 })
 export class CommonPrimengModule { }
