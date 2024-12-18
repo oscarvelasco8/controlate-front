7f2724c13a7adeb559c84c9072f97de940b93b2f
oscar.velasco <oscar.velasco@madisonmk.com>
2024-12-18 14:14:44 +0100

    fix url

--- src/app/shared/services/food.service.ts
+++ src/app/shared/services/food.service.ts
@@ -9,8 +9,8 @@ import {FoodInfo} from '../interfaces/FoodInfo';
   providedIn: 'root'
 })
 export class FoodService{
-  //private BASE_URL = 'https://controlate-back.koyeb.app/api';
-  private BASE_URL = 'http://localhost:8080/api';
+  private BASE_URL = 'https://controlate-back.koyeb.app/api';
+  //private BASE_URL = 'http://localhost:8080/api';
   private _foodsInfo:WritableSignal<FoodInfo[]>= signal([
     {
       name:'pollo',
